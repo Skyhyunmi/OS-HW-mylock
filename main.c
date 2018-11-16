3032386149cc7abfc8e4350592ed727fd316e72b
codikh@ajou.ac.kr <codikh@ajou.ac.kr>
2018-11-16 18:03:16 +0900

    보고서 업로드

--- main.c
+++ main.c
@@ -65,34 +65,34 @@ int parse_command(int argc, char *argv[])
 			nr_generators = 8;
 			nr_generate = (1 << 16);
 			lock_type = lock_spinlock;
-			verbose = 1;
+			verbose = 0;
 			break;
 		case '1':
 			generator_type = generator_random;
 			nr_generators = 8;
 			nr_generate = (1 << 16);
 			lock_type = lock_mutex;
-			verbose = 1;
+			verbose = 0;
 			break;
 		case '2':
 			generator_type = generator_random;
 			nr_generators = 8;
 			nr_generate = (1 << 16);
 			lock_type = lock_semaphore;
-			verbose = 1;
+			verbose = 0;
 			break;
 		case '3': /* Overflow test */
 			generator_type = generator_random;
 			nr_generators = 8;
 			nr_generate = (1 << 10);
 			counter_type = counter_delayed;
-			verbose = 1;
+			verbose = 0;
 			break;
 		case '4': /* Underflow test */
 			generator_type = generator_delayed;
 			nr_generators = 8;
 			nr_generate = (1 << 10);
-			verbose = 1;
+			verbose = 0;
 			break;
 		case 'h':
 		case '?':
